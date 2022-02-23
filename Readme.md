c5fdb55a1ed4c5cd3a39f05428040ce9524caae7
ccchen-jerry <npc4sell@gmail.com>
2022-02-23 19:39:12 +0800

    add files

---
+++ Readme.md
@@ -0,0 +1,49 @@
+# Basic Upload File Using Python Flask
+
+### Environment
+
+- Mac mini M1
+- Python 3.10
+
+### Start the pipenv
+
+1. Create project directory and navigate into it
+
+   ```shell
+   ~ % mkdir flask_request_example
+   ~ % cd flask_request_example
+   ```
+
+2. (If not installed) Install packages
+
+   ```shell
+   ~ % python3 -m pip pipenv
+   ```
+
+3. Install Flask
+
+   ```shell
+   ~ % pipenv install Flask
+   ```
+
+   The pipenv will create a virtualenv for the project automatically and then install Flask.
+
+4. Activate the virtual environment
+
+   ```shell
+   ~ % pipenv shell
+   ```
+
+   And you'll see `(flask_request_example)` in front of the command line.
+
+5. Create an app.py to start!
+
+### Start the service
+
+```shell
+(flask_request_example) ~ % python app.py
+```
+
+### 
+
+Please edit line 11 before runing.
---
+++ app.py
@@ -0,0 +1,120 @@
+import os
+# import main Flask class and request object
+from flask import Flask, redirect, request, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+# create the Flask app
+app = Flask(__name__)
+
+# define app setting
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = "EDIT_YOUR_DIRECTORY_HERE/flask_request_example/upload_files"
+app.config['MAX_CONTENT_PATH'] = 10 * 1024 * 1024
+
+# check extensions
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 
+                                   filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        else:
+            return "file not supported"
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route("/upload_files/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route("/query-example")
+def query_example():
+    # if key doesn't exsit, returns None
+    language = request.args.get('language')
+
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args['framework']
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+              <h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
+
+# allow both GET and POST requests
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form.get('framework')
+        return '''
+                  <h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>Language: <input type="text" name="language"></label></div>
+               <div><label>Framework: <input type="text" name="framework"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''
+
+# GET requests will be blocked
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    language = None
+    framework = None
+    python_version = None
+    example = None
+    boolean_test = None
+
+    if request_data:
+        if 'language' in request_data:
+            language = request_data['language']
+
+        if 'framework' in request_data:
+            framework = request_data['framework']
+
+        if 'version_info' in request_data:
+            if 'python' in request_data['version_info']:
+                python_version = request_data['version_info']['python']
+
+        if 'examples' in request_data:
+            if (type(request_data['examples']) == list) and (len(request_data['examples']) > 0):
+                example = request_data['examples'][0]
+
+        if 'boolean_test' in request_data:
+            boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
+
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
+
